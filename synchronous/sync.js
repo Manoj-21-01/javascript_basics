b8f52baa8442330dbf6e988c94eb0a8533fe6902
Manoj Veluru <manojveluru21@gmail.com>
2024-01-21 23:37:15 +0530

    [MANOJ] Add: Added files

--- synchronous/sync.js
+++ synchronous/sync.js
@@ -1,3 +1,4 @@
+//program 1
 function f1(){
     console.log("f1");
 }
@@ -8,3 +9,22 @@ function f2(){
 
 f1();
 f2();
+
+//program 2
+
+function f1(){
+    let name = "Nani";
+    console.log("Hi " + name);
+}
+
+function f2(){
+    console.log("greetings");
+}
+
+function main(){
+    console.log("How it's going");
+}
+
+f2();
+f1();
+main();
