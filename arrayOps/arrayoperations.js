8f795d96b4e6970741f0e01e2949c2eae6c4a4fc
Manoj Veluru <manojveluru21@gmail.com>
2024-01-23 11:02:47 +0530

    [MANOJ] Add: Added file

--- arrayOps/arrayoperations.js
+++ arrayOps/arrayoperations.js
@@ -1,1 +1,38 @@
-console.log(new Date());
+//array declaration
+
+const months = ["April", "May", "June"];
+console.log(months);
+
+const names = new Array("Nani", "David", "Rahul"); //new Array also does the same operation
+console.log(names);
+
+//arrayToString
+
+const things = ["Laptop", "Book", "Cable"];
+let res = things.toString();
+console.log(res);
+console.log(typeof(things)); //returns type of array: Object
+console.log(things.sort());
+console.log(things.length);
+console.log(things[0]);
+const personDetails = {firstName: "Manoj",lastName:"Veluru",age:21};
+console.log(personDetails.firstName);
+
+const fruits = ["Apple","Banana","Mango","Kiwi"];
+let flen = fruits.length;
+for (let i=0;i<flen;i++){
+    console.log(fruits[i]);
+}
+
+//another way of using for loop to loop the elements
+const num = [1,2,3,4,5];
+for (const elements of num){
+    console.log(elements);
+}
+
+//new element can be added to array using push or length property
+let colors = ["Blue","Green","Yellow"];
+colors.push("Violet");
+colors[colors.length] = "Red";
+console.log(colors)
+console.log(Array.isArray(colors)); //returns true
